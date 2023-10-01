68915c07f108acaa029fa1b5db1825594f4980e2
Sangeet Kumar <91060606+skdeveloper8@users.noreply.github.com>
2023-10-01 18:48:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {useState} from 'react';
+import data from './data'
+import Tours from './components/Tours';
+import './App.css';
+
+function App() {
+  const [tours,setTours]=useState(data)
+  function removeTour(id){
+    const newTours=tours.filter(tour=>tour.id !==id);
+    setTours(newTours);
+        
+  }
+  if(tours.length===0){
+    return(
+      <div className='refresh'>
+     <h2>No tours left</h2>
+     <button className='btn-white' onClick={()=>setTours(data)}>Refresh</button>
+      </div>
+    );
+  }
+  return (
+    <div >
+  
+    <Tours tours={tours} removeTour={removeTour}></Tours>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,53 @@
+
+const data=[
+    {
+        id:1,
+        name:"Agra",
+        info:"Agra's notable historical period began during Sikandar Lodi's reign, but the golden age of the city began with the Mughals. Agra was the foremost city of the Indian subcontinent and the capital of the Mughal Empire under Mughal emperors Babur, Humayun, Akbar, Jahangir and Shah Jahan. Under Mughal rule, Agra became a centre for learning, arts, commerce, and religion, and saw the construction of the Agra Fort, Sikandra and Agra's most prized monument, the Taj Mahal, built by Shah Jahan as a mausoleum for his favourite empress. With the decline of the Mughal empire in the late 18th century, the city fell successively first to Marathas and later to the East India Company. After Independence, Agra has developed into an industrial town, with a booming tourism industry, along with footwear, leather and other manufacturing. The Taj Mahal and the Agra Fort are UNESCO World Heritage Sites. The city features mild winters, hot and dry summers and a monsoon season, and is famous for its Mughlai cuisine. Agra is included on the Golden Triangle tourist circuit, along with Delhi and Jaipur; and the Uttar Pradesh Heritage Arc, a tourist circuit of Uttar Pradesh, along with Lucknow and Varanasi. ",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Taj_Mahal%2C_Agra%2C_India.jpg/800px-Taj_Mahal%2C_Agra%2C_India.jpg",
+        price:"10,000",
+    },
+    {
+        id:2,
+        name:"Agra2",
+        info:"Agra's notable historical period began during Sikandar Lodi's reign, but the golden age of the city began with the Mughals. Agra was the foremost city of the Indian subcontinent and the capital of the Mughal Empire under Mughal emperors Babur, Humayun, Akbar, Jahangir and Shah Jahan. Under Mughal rule, Agra became a centre for learning, arts, commerce, and religion, and saw the construction of the Agra Fort, Sikandra and Agra's most prized monument, the Taj Mahal, built by Shah Jahan as a mausoleum for his favourite empress. With the decline of the Mughal empire in the late 18th century, the city fell successively first to Marathas and later to the East India Company. After Independence, Agra has developed into an industrial town, with a booming tourism industry, along with footwear, leather and other manufacturing. The Taj Mahal and the Agra Fort are UNESCO World Heritage Sites. The city features mild winters, hot and dry summers and a monsoon season, and is famous for its Mughlai cuisine. Agra is included on the Golden Triangle tourist circuit, along with Delhi and Jaipur; and the Uttar Pradesh Heritage Arc, a tourist circuit of Uttar Pradesh, along with Lucknow and Varanasi. ",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Jaswant_Singh-ki-Chattri.jpg/800px-Jaswant_Singh-ki-Chattri.jpg",
+        price:"19,000",
+    },
+    {
+        id:3,
+        name:"Agra3",
+        info:"Agra's notable historical period began during Sikandar Lodi's reign, but the golden age of the city began with the Mughals. Agra was the foremost city of the Indian subcontinent and the capital of the Mughal Empire under Mughal emperors Babur, Humayun, Akbar, Jahangir and Shah Jahan. Under Mughal rule, Agra became a centre for learning, arts, commerce, and religion, and saw the construction of the Agra Fort, Sikandra and Agra's most prized monument, the Taj Mahal, built by Shah Jahan as a mausoleum for his favourite empress. With the decline of the Mughal empire in the late 18th century, the city fell successively first to Marathas and later to the East India Company. After Independence, Agra has developed into an industrial town, with a booming tourism industry, along with footwear, leather and other manufacturing. The Taj Mahal and the Agra Fort are UNESCO World Heritage Sites. The city features mild winters, hot and dry summers and a monsoon season, and is famous for its Mughlai cuisine. Agra is included on the Golden Triangle tourist circuit, along with Delhi and Jaipur; and the Uttar Pradesh Heritage Arc, a tourist circuit of Uttar Pradesh, along with Lucknow and Varanasi. ",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Agra_03-2016_16_Agra_Fort.jpg/800px-Agra_03-2016_16_Agra_Fort.jpg",
+        price:"16,000",
+    },
+    {
+        id:4,
+        name:"Agra4",
+        info:"Agra's notable historical period began during Sikandar Lodi's reign, but the golden age of the city began with the Mughals. Agra was the foremost city of the Indian subcontinent and the capital of the Mughal Empire under Mughal emperors Babur, Humayun, Akbar, Jahangir and Shah Jahan. Under Mughal rule, Agra became a centre for learning, arts, commerce, and religion, and saw the construction of the Agra Fort, Sikandra and Agra's most prized monument, the Taj Mahal, built by Shah Jahan as a mausoleum for his favourite empress. With the decline of the Mughal empire in the late 18th century, the city fell successively first to Marathas and later to the East India Company. After Independence, Agra has developed into an industrial town, with a booming tourism industry, along with footwear, leather and other manufacturing. The Taj Mahal and the Agra Fort are UNESCO World Heritage Sites. The city features mild winters, hot and dry summers and a monsoon season, and is famous for its Mughlai cuisine. Agra is included on the Golden Triangle tourist circuit, along with Delhi and Jaipur; and the Uttar Pradesh Heritage Arc, a tourist circuit of Uttar Pradesh, along with Lucknow and Varanasi. ",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Tomb_of_Akbar_the_Great.jpg/800px-Tomb_of_Akbar_the_Great.jpg",
+        price:"15,000",
+    },
+    {
+        id:5,
+        name:"Agra5",
+        info:"Agra's notable historical period began during Sikandar Lodi's reign, but the golden age of the city began with the Mughals. Agra was the foremost city of the Indian subcontinent and the capital of the Mughal Empire under Mughal emperors Babur, Humayun, Akbar, Jahangir and Shah Jahan. Under Mughal rule, Agra became a centre for learning, arts, commerce, and religion, and saw the construction of the Agra Fort, Sikandra and Agra's most prized monument, the Taj Mahal, built by Shah Jahan as a mausoleum for his favourite empress. With the decline of the Mughal empire in the late 18th century, the city fell successively first to Marathas and later to the East India Company. After Independence, Agra has developed into an industrial town, with a booming tourism industry, along with footwear, leather and other manufacturing. The Taj Mahal and the Agra Fort are UNESCO World Heritage Sites. The city features mild winters, hot and dry summers and a monsoon season, and is famous for its Mughlai cuisine. Agra is included on the Golden Triangle tourist circuit, along with Delhi and Jaipur; and the Uttar Pradesh Heritage Arc, a tourist circuit of Uttar Pradesh, along with Lucknow and Varanasi. ",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Guru_ka_Tal_wikijib-15.jpg/800px-Guru_ka_Tal_wikijib-15.jpg",
+        price:"14,000",
+    },
+    {
+        id:6,
+        name:"Agra6",
+        info:"Agra's notable historical period began during Sikandar Lodi's reign, but the golden age of the city began with the Mughals. Agra was the foremost city of the Indian subcontinent and the capital of the Mughal Empire under Mughal emperors Babur, Humayun, Akbar, Jahangir and Shah Jahan. Under Mughal rule, Agra became a centre for learning, arts, commerce, and religion, and saw the construction of the Agra Fort, Sikandra and Agra's most prized monument, the Taj Mahal, built by Shah Jahan as a mausoleum for his favourite empress. With the decline of the Mughal empire in the late 18th century, the city fell successively first to Marathas and later to the East India Company. After Independence, Agra has developed into an industrial town, with a booming tourism industry, along with footwear, leather and other manufacturing. The Taj Mahal and the Agra Fort are UNESCO World Heritage Sites. The city features mild winters, hot and dry summers and a monsoon season, and is famous for its Mughlai cuisine. Agra is included on the Golden Triangle tourist circuit, along with Delhi and Jaipur; and the Uttar Pradesh Heritage Arc, a tourist circuit of Uttar Pradesh, along with Lucknow and Varanasi. ",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Agra_33_-_Shivaji_monument_in_front_of_Red_Fort_%2841541115414%29.jpg/800px-Agra_33_-_Shivaji_monument_in_front_of_Red_Fort_%2841541115414%29.jpg",
+        price:"13,000",
+    },
+    {
+        id:7,
+        name:"Agra7",
+        info:"Agra's notable historical period began during Sikandar Lodi's reign, but the golden age of the city began with the Mughals. Agra was the foremost city of the Indian subcontinent and the capital of the Mughal Empire under Mughal emperors Babur, Humayun, Akbar, Jahangir and Shah Jahan. Under Mughal rule, Agra became a centre for learning, arts, commerce, and religion, and saw the construction of the Agra Fort, Sikandra and Agra's most prized monument, the Taj Mahal, built by Shah Jahan as a mausoleum for his favourite empress. With the decline of the Mughal empire in the late 18th century, the city fell successively first to Marathas and later to the East India Company. After Independence, Agra has developed into an industrial town, with a booming tourism industry, along with footwear, leather and other manufacturing. The Taj Mahal and the Agra Fort are UNESCO World Heritage Sites. The city features mild winters, hot and dry summers and a monsoon season, and is famous for its Mughlai cuisine. Agra is included on the Golden Triangle tourist circuit, along with Delhi and Jaipur; and the Uttar Pradesh Heritage Arc, a tourist circuit of Uttar Pradesh, along with Lucknow and Varanasi. ",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Mariam%27s_Tomb%2C_Sikandra%2C_Agra.JPG/1024px-Mariam%27s_Tomb%2C_Sikandra%2C_Agra.JPG",
+        price:"12,000",
+    },
+]
+export default data;
